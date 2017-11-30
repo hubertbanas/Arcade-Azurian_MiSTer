2da011c4e29df4671bd6629e776599d62269d136
sorgelig <pour.garbage@gmail.com>
2017-12-01 03:32:42 +0800

    Initial implementation.

---
+++ releases/build_rom_Azurian.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set    zip=azurian.zip
+set ifiles=pgm.1+pgm.2+pgm.3+pgm.3+gfx.1+gfx.1+gfx.2+gfx.2
+set  ofile=a.azurn.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
